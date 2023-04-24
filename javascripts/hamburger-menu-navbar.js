f109fab12e794f6b2c0e487971525d40230b0fc4
Quinn Montgomery <quinninthetardis@gmail.com>
2023-04-24 11:57:29 -0400

    fixed bug where mobile nav bar would break

--- javascripts/hamburger-menu-navbar.js
+++ javascripts/hamburger-menu-navbar.js
@@ -3,19 +3,21 @@ $(document).ready(function () {
     $(".navbar-toggle").click(function () {
 
         $("#my_navbar").fadeToggle(100);
-        $("#main_menu").removeClass('move');
 
     });
 
     $('.dropdown-toggle').on('click', function () {
         
         $(this).parent().parent().toggleClass('move');
+        $(this).parent().toggleClass('open');
+        $(this).lastChild().fadeToggle(0);
 
     });
     
     $('.return-lvl').on('click', function () {
 
         $(this).parent().parent().parent().parent().removeClass('move');
+        $(this).parent().parent().parent().removeClass('open');
 
     });
 
